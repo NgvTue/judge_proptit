94dbdac542a8cb50b0981b873c67bb683fc868cf
Your Name <tue.nguyen.2411@gmail.com>
2021-01-13 10:43:29 +0700

    add everything

---
+++ core_judge/test/test_output_only.py
@@ -0,0 +1,32 @@
+try:
+
+    from ..job.OutputOnly import OutputOnly
+
+except:
+    from job.OutputOnly import OutputOnly
+    from config import env
+
+contestant_source = []
+with open("/opt/source_code_for_testing/par.cpp","r") as f:
+    contestant_source = f.read()
+
+
+
+job_description = {
+    "multiprocess":False,
+    "time":1.,
+    "contestant_source":contestant_source,
+    "contestant_lang":"cpp",
+    "checker":"/opt/problems_test/4/checker.cpp",
+    "checker_lang":"cpp",
+    "inputs":"/opt/problems_test/4/inputs",
+    "outputs":"/opt/problems_test/4/outputs",
+    "type":"ioi",
+    "sess":"submit",
+}
+
+wo = OutputOnly(job_description)
+# wo.prepare_file()
+# wo.compiler()
+wo.run()
+# wo.sandbox.cleanup()
---
+++ readme.md
@@ -0,0 +1,17 @@
+
+judge test proptit
+
+REFERENCES:
+
+    IOI-ISOLATE: [ISOLATE](https://github.com/ioi/isolate)
+
+    CMS-DEV : [CMS-DEV](https://cms-dev.github.io/)
+
+---
+
+PROVIDE:
+
+1. OUTPUT_INPUT_STANDART_JUDGE
+2. ITERACTIVE_JUDGE
+3. VALDATOR_JUDGE
+4. CHECKER WITH TESTLIB
